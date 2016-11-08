c14fb9c71eba323065ba3116b262b5ae8999ea77
Carson.Ho <carson.ho@foxmail.com>
2016-11-08 16:43:42 +0800

    ’将本地服务设置成前台服务‘

--- app/src/main/java/scut/carson_ho/demo_service/MyService.java
+++ app/src/main/java/scut/carson_ho/demo_service/MyService.java
@@ -1,5 +1,7 @@
 package scut.carson_ho.demo_service;
 
+import android.app.Notification;
+import android.app.PendingIntent;
 import android.app.Service;
 import android.content.Intent;
 import android.os.Binder;
@@ -16,7 +18,27 @@ public class MyService extends Service {
     @Override
     public void onCreate() {
         super.onCreate();
+
         System.out.println("执行了onCreat()");
+
+        //变成前台Service
+
+
+
+        //构建"点击通知后打开MainActivity"的Intent对象
+        Intent notificationIntent = new Intent(this,MainActivity.class);
+        PendingIntent pendingIntent = PendingIntent.getActivity(this,0,notificationIntent,0);
+
+        //新建Builer对象
+        Notification.Builder builer = new Notification.Builder(this);
+        builer.setContentTitle("前台服务通知的标题");//设置通知的标题
+        builer.setContentText("前台服务通知的内容");//设置通知的内容
+        builer.setSmallIcon(R.mipmap.ic_launcher);//设置通知的图标
+        builer.setContentIntent(pendingIntent);//设置点击通知后的操作
+
+        Notification notification = builer.getNotification();//将Builder对象转变成普通的notification
+        startForeground(1, notification);//让Service变成前台Service,并在系统的状态栏显示出来
+
     }
 
     @Override
